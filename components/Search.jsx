d53a473932d0900f285c7ca2a0e30908f53b5ed8
shvkumar7777 <43500125+shvkumar7777@users.noreply.github.com>
2023-03-07 18:08:19 +0530

    3 files modified

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 import './Search.css';
+import Button from '@mui/material/Button';
+
 function Search() {
+  const [input, setInput] = useState('');
+
+  const search = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="search">
       <div className="search__input">
-        <SearchIcon />
-        <input type="text" />
+        <SearchIcon className="search__inputIcon" />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <KeyboardVoiceIcon />
+      </div>
+      <div className="search__buttons">
+        <Button variant="outlined" onClick={search}>
+          Google Search
+        </Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
